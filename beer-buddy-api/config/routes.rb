991e89302a31bfd4f6af6c69231729d59b579aec
Joey Mckenzie <joey.mckenzie27@gmail.com>
2021-07-01 20:11:23 -0700

    feat(Api): Add API namespace for resource controllers

--- beer-buddy-api/config/routes.rb
+++ beer-buddy-api/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :beers
-  resources :addresses
-  resources :breweries
+  namespace :api do
+    resources :beers
+    resources :addresses
+    resources :breweries
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
